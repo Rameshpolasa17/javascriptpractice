9f7952a638b84c13e411b87819e71f61e31133d6
Rameshpolasa17 <149149368+Rameshpolasa17@users.noreply.github.com>
2023-10-31 14:19:35 +0000

    commit

---
+++ javascript/04mathandnum.js
@@ -0,0 +1,23 @@
+/*let score=200
+let balance= new Number(20)
+console.log(score)
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(4))
+let otherNumber=28.269
+console.log(otherNumber.toPrecision(4))
+let hundred=1000000
+console.log(hundred.toLocaleString('en-IN'))*/
+
+
+//-----------Math----------------
+console.log(Math.abs(-4))
+console.log(Math.ceil(2.3))
+console.log(Math.floor(2.8))
+console.log(Math.max(3,5,4,2,8))
+console.log(Math.min(4,5,3,7,1,9))
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10)+1)
+let min=2
+let max=12
+console.log(Math.floor(Math.random()*(max-min+1))+min)
