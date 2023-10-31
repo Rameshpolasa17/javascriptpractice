48be4f5c64adf98c13b21e2ffc5f305ce116fbc3
Rameshpolasa17 <149149368+Rameshpolasa17@users.noreply.github.com>
2023-10-31 10:30:05 +0000

    commit

---
+++ javascript/03strings.js
@@ -0,0 +1,12 @@
+let name="Ramesh"
+let repoCount=2
+console.log(`Hi my name is ${name} and my repo count is ${repoCount}`)
+let gameName = new String("Ramesh")
+console.log(gameName[0])
+console.log(gameName.length)
+let newName=gameName.substring(0,4)
+console.log(newName)
+console.log(gameName)
+let newwName="Ramesh/polasa"
+console.log(newwName.replace('/','-'))
+console.log(newwName.includes("Ramesh"))
