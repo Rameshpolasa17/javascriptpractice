4512c38168cf6e694ab3511132729b765f7a2dbc
Rameshpolasa17 <149149368+Rameshpolasa17@users.noreply.github.com>
2023-11-04 14:47:46 +0000

    commit

---
+++ javascript/09scopes.js
@@ -0,0 +1,13 @@
+// let a=10
+// const b=20
+// var c=30
+// let a=60 //Global scope can be access anywhere
+// if (true) {
+//     let a=10 //Local variable
+//     const b=20
+//     var c=30 //var variable used as global scope within the block
+//     console.log(a)
+// }
+// console.log(a)
+// // console.log(b)
+// console.log(c)
