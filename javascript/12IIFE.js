fd9afa3c7b91cf7133993b0f3ed12a71e419916f
Rameshpolasa17 <149149368+Rameshpolasa17@users.noreply.github.com>
2023-11-05 08:15:30 +0000

    commit

---
+++ javascript/12IIFE.js
@@ -0,0 +1,10 @@
+//Immediately invoked function expression
+// (function chai(){
+//     console.log("Hello") //This is named iife
+// })(); //Semicolon to stop the invoked function
+
+// ((name)=>{
+//     console.log(`Hi ${name}`) //This is unnamed iife
+// })("Ramesh");
+
+
