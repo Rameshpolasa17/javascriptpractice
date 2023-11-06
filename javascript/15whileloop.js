3bcc121e9e57953ae9b8dff535173fbd35b7d65f
Rameshpolasa17 <149149368+Rameshpolasa17@users.noreply.github.com>
2023-11-06 15:37:09 +0000

    commit

--- javascript/15whileloop.js
+++ javascript/15whileloop.js
@@ -1,27 +1,27 @@
-// let index=0
-// while (index<=10) {
-//     console.log(index)
-//     index++
-// }
+let index=0
+while (index<=10) {
+    console.log(index)
+    index++
+}
 
-// let Myarr=["Virat", "Rohit", "Shreyas"]
-// let arr=0
-// while (arr<Myarr.length) {
-//     console.log(Myarr[arr])
-//     arr++
-// }
+let Myarr=["Virat", "Rohit", "Shreyas"]
+let arr=0
+while (arr<Myarr.length) {
+    console.log(Myarr[arr])
+    arr++
+}
 
-// let favCric=["Virat Kohli", "Suresh Raina", "MahendraSingh Dhoni"]
-// let fav=0
-// while (fav<favCric.length) {
-//     console.log(`My favourite cricketer is ${favCric[fav]}`)
-//     fav++
-// }
+let favCric=["Virat Kohli", "Suresh Raina", "MahendraSingh Dhoni"]
+let fav=0
+while (fav<favCric.length) {
+    console.log(`My favourite cricketer is ${favCric[fav]}`)
+    fav++
+}
 
 //Do while loop------------------------
 
-// let score=11
-// do {
-//     console.log(`The value is ${score}`)
-//     score++
-// } while (score<10);
+let score=11
+do {
+    console.log(`The value is ${score}`)
+    score++
+} while (score<10);
