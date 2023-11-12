5cd9740f5e0c05aec29085da95a568128e10b1b4
Rameshpolasa17 <149149368+Rameshpolasa17@users.noreply.github.com>
2023-11-12 18:48:56 +0000

    commit

---
+++ projects/09_diwali.js
@@ -0,0 +1,6 @@
+const changeText=function(){
+    document.querySelector('.greeting').innerHTML="May this Diwali bring you all the happiness and joy in the world! "
+    
+}
+// changeFontsize.innerHTML=function(){}
+setTimeout(changeText,4000)
